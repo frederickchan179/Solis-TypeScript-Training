5e013b60148130010574b197018815b0481a587b
Frederick Chan <hoa.tran@solislab.com>
2023-08-16 22:42:28 +0700

    :bug: Answer day1 quiz and question

---
+++ answers/day1/warm-up.fred2.ts
@@ -0,0 +1,19 @@
+// Day 1 - Warm Up
+
+// ============= Test Cases =============
+import type { Equal, Expect, NotAny } from '@type-challenges/utils'
+
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+type cases = [
+  Expect<NotAny<HelloWorld>>,
+  Expect<Equal<HelloWorld, string>>,
+  Expect<Equal<HelloWorld, typeof str>>,
+  Expect<Equal<typeof constStr, typeof str>>
+]
+
+// ============= Your Code Here =============
+type HelloWorld = string
+const constStr = 'Hi world' as string
+
+// eslint-disable-next-line prefer-const
+let str = 'Hello World'
